3c6324b0469e2dc9b7408f8297f1dce6ff0ba8f6
Tennessene <ansorensen1118@gmail.com>
2022-05-01 13:42:38 -0500

    Added last good plugin
    Added the last good version of the plugin for the old-last-good branch.

--- src/main/java/me/acclashcorporation/updatechecker/UpdateChecker.java
+++ src/main/java/me/acclashcorporation/updatechecker/UpdateChecker.java
@@ -1,67 +1,68 @@
 package me.acclashcorporation.updatechecker;
 
-import me.acclashcorporation.updatechecker.commands.Check;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public final class UpdateChecker extends JavaPlugin {
 
-    @Override
     public void onEnable() {
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
-        getCommand("check").setExecutor(new Check(this));
-        String mcversion = Bukkit.getBukkitVersion();
-        mcversion = mcversion.substring(mcversion.indexOf("1.") + 2);
-        mcversion = mcversion.substring(0, mcversion.indexOf(".") + 2);
-        if (Bukkit.getVersion().contains("Spigot")) {
-            CheckSpigot.checkSpigot(mcversion);
-        } else if (Bukkit.getVersion().contains("Paper")) {
-            //BROKE NEEDS FIXED
-            //if (getConfig().getString("Start script") == null) {
-            //    getLogger().severe("You must enter a valid start script name in the configuration file to use this plugin!");
-            //    getServer().getPluginManager().disablePlugin(this);
-            //} else {
-                CheckPaper.checkPaper(mcversion);
-            //}
-        } else {
-            CheckPaper.checkPaper(mcversion);
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        if (Bukkit.getVersion().contains("Spigot")) {
-            String mcversion = Bukkit.getBukkitVersion();
-            mcversion = mcversion.substring(mcversion.indexOf("1.") + 2);
-            mcversion = mcversion.substring(0, mcversion.indexOf(".") + 2);
-            File JAR = new File("BuildTools" + File.separator + "spigot-1." + mcversion + ".jar");
-            File finalJAR = new File("spigot-1." + mcversion + " (new).jar");
-            try {
-                String OS = System.getProperty("os.name").toLowerCase();
-                if (OS.contains("win")) {
-                    if (finalJAR.exists()) {
-                        PrintWriter writer = new PrintWriter("Update.bat", StandardCharsets.UTF_8);
-                        writer.println("@echo off");
-                        writer.println("ren \"" + JAR + "\" \"" + finalJAR + "\"");
-                        writer.close();
-                        Runtime.getRuntime().exec("cmd /c start /min \"\" Update.bat");
-                    }
-                } else if (OS.contains("nix") || OS.contains("nux") || OS.contains("aix")) {
-                    PrintWriter writer = new PrintWriter("Update.sh", StandardCharsets.UTF_8);
-                    writer.println("#!/bin/bash");
-                    writer.println("mv \"" + JAR + "\" \"" + finalJAR + "\"");
-                    writer.close();
-                    Runtime.getRuntime().exec("bash Update.sh");
-                }
-                // MAC SUPPORT HERE?
-            } catch (IOException ignored) {
+        File folder = new File(System.getProperty("user.dir"));
+        File fList[] = folder.listFiles();
+        for (int i = 0; i < fList.length; i++) {
+            String oldjar = String.valueOf(fList[i]);
+            if (oldjar.endsWith(".jar")) {
+                boolean success = (new File(String.valueOf(fList[i])).delete());
             }
         }
+        String lversion = null;
+        try {
+            String out = new Scanner(new URL("https://papermc.io/api/v2/projects/paper/versions/1.18.2/").openStream(), "UTF-8").useDelimiter("\\A").next();
+            String lstring = out.substring(out.lastIndexOf(",") + 1);
+            lversion = lstring.replaceAll("[^0-9]", "");
+        } catch (IOException e) {
+            this.getLogger().info(ChatColor.RED + "No internet: Unable to retrieve Paper versions.");
+        }
+
+        String version = Bukkit.getVersion();
+        String numberOnly = version.replaceAll("[^0-9]", "");
+        String bversion = numberOnly.substring(0, numberOnly.length() - 4);
+        if (bversion.equals(lversion)) {
+            this.getLogger().info(ChatColor.GREEN + "Your Paper software is up to date.");
+        } else {
+            this.getLogger().info(ChatColor.YELLOW + "Your Paper software isn't up to date(revision: " + bversion + " latest: " + lversion + "). Updating now...");
+
+            try {
+                URL website = new URL("https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/" + lversion + "/downloads/paper-1.18.2-" + lversion + ".jar");
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream("paper-1.18.2-" + lversion + ".jar");
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                Path path = Paths.get(System.getProperty("user.dir") + "\\ServerStart1gb.bat");
+                Charset charset = StandardCharsets.UTF_8;
+                String content = new String(Files.readAllBytes(path), charset);
+                content = content.replaceAll("paper-1.18.2-" + bversion + ".jar", "paper-1.18.2-" + lversion + ".jar");
+                Files.write(path, content.getBytes(charset));
+                this.getLogger().info(ChatColor.GREEN + "Paper software updated to revision " + lversion + ". It will take effect next server launch.");
+            } catch (IOException e) {
+                this.getLogger().info(ChatColor.RED + "No internet: Unable to download Paper software.");
+            }
+        }
+
+    }
+
+    public void onDisable() {
     }
 }
